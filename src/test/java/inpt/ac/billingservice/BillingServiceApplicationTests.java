4b8addb729c819abcb452d75dc61e2a53525323f
amineabdelmoumen <abdelmoumen33330@gmail.com>
2021-11-29 19:35:17 +0100

    adding billing servc

--- src/test/java/inpt/ac/billingservice/BillingServiceApplicationTests.java
+++ src/test/java/inpt/ac/billingservice/BillingServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package inpt.ac.discoveryservice;
+package inpt.ac.billingservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DiscoveryServiceApplicationTests {
+class BillingServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
